c79e87971efe77377f02d30abbad30f2c3833688
Alex <Alexander.Bashlykov@sitewerk.ch>
2025-02-19 22:59:36 -0300

    add Circle tool

--- front/src/components/Toolbar.jsx
+++ front/src/components/Toolbar.jsx
@@ -3,6 +3,7 @@ import '../styles/toolbar.scss';
 import toolState from '../store/toolState';
 import Brush from '../tools/Brush';
 import Rect from '../tools/Rect';
+import Circle from '../tools/Circle';
 import canvasState from '../store/canvasState';
 
 const Toolbar = () => {
@@ -10,7 +11,7 @@ const Toolbar = () => {
     <div className='toolbar'>
       <button className='toolbar__btn brush' onClick={() => toolState.setTool(new Brush(canvasState.canvas))} />
       <button className="toolbar__btn rect" onClick={() => toolState.setTool(new Rect(canvasState.canvas))} />
-      <button className="toolbar__btn circle" />
+      <button className="toolbar__btn circle" onClick={() => toolState.setTool(new Circle(canvasState.canvas))}  />
       <button className="toolbar__btn eraser" />
       <button className="toolbar__btn line" />
       <input style={{marginLeft:10}} type="color"/>
---
+++ front/src/tools/Circle.js
@@ -0,0 +1,52 @@
+import Tool from "./Tool";
+
+export default class Circle extends Tool {
+  constructor(canvas) {
+    super(canvas);
+    this.listen();
+  }
+
+  listen() {
+    this.canvas.onmousedown = this.mouseDownHandler.bind(this);
+    this.canvas.onmouseup = this.mouseUpHandler.bind(this);
+    this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
+  }
+
+  distanceTo(point1, point2) {
+    return Math.sqrt((Math.pow(point2.x-point1.x,2))+(Math.pow(point2.y-point1.y,2)))
+  };
+
+  mouseUpHandler(e) {
+    this.mouseDown = false;
+  }
+
+  mouseDownHandler(e) { 
+    this.mouseDown = true;
+    this.ctx.beginPath();
+    this.startX = e.pageX - e.target.offsetLeft;
+    this.startY = e.pageY - e.target.offsetTop;
+    this.saved = this.canvas.toDataURL();
+  }
+
+  mouseMoveHandler(e) {
+    if (this.mouseDown) {
+      let currentX = e.pageX - e.target.offsetLeft;
+      let currentY = e.pageY - e.target.offsetTop;
+      let radius = this.distanceTo({x: this.startX, y: this.startY}, {x: currentX, y: currentY});
+      this.draw(this.startX, this.startY, radius);
+    }
+  }
+
+  draw(x, y, r) {
+    const img = new Image();
+    img.src = this.saved;
+    img.onload = () => {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.beginPath();
+      this.ctx.arc(x, y, r, 0, Math.PI * 2);
+      this.ctx.fill();
+      this.ctx.stroke();
+    }
+  }
+}
