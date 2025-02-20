3b4fc2886c692f1454c5ed13866eb55639b01b07
Alex <Alexander.Bashlykov@sitewerk.ch>
2025-02-20 15:15:11 -0300

    add margin to the stroke color input

--- front/src/components/SettingBar.jsx
+++ front/src/components/SettingBar.jsx
@@ -13,8 +13,12 @@ const SettingBar = () => {
           style={{margin: '0 10px'}}
           id="line-width"
           type="number" defaultValue={1} min={1} max={50}/>
-      <label htmlFor="stroke-color">Stroke color</label>
-      <input onChange={e => toolState.setStrokeColor(e.target.value)} id="stroke-color" type="color"/>
+      <label htmlFor="stroke-color" >Stroke color</label>
+      <input 
+        onChange={e => toolState.setStrokeColor(e.target.value)} 
+        style={{margin: '0 10px'}}
+        id="stroke-color" 
+        type="color"/>
     </div>
   );
 };
