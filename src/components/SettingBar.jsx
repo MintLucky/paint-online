ae38a1079196de47149cb2f4fb39ccf7db1560c8
Alex <Alexander.Bashlykov@sitewerk.ch>
2025-02-19 21:37:45 -0300

    first commit

---
+++ src/components/SettingBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/toolbar.scss';
+
+const SettingBar = () => {
+  return (
+    <div className='toolbar setting-bar'>
+      Setting
+    </div>
+  );
+};
+
+export default SettingBar;
